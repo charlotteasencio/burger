b3c0e1a0b05d809aa1dbe24eced319b2965cb845
Charlotte Asencio <charlotte.e.asencio@gmail.com>
2018-06-21 12:50:16 -0400

    setting up congig

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burger (burger_name, devoured)
+VALUES ("bacon cheese burger", 0);
+
+INSERT INTO burger (burger_name, devoured)
+VALUES("blue cheese", 1);
+
+INSERT INTO burger (burger_name, devoured)
+VALUES("cheese burger", 0);
